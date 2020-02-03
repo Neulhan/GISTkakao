1c2a384bf7b4cc282541bbb309126743a3941a26
신한결 <sting7675@naver.com>
2020-02-03 13:31:10 +0900

    hotfix

--- gist_kakao_api/urls.py
+++ gist_kakao_api/urls.py
@@ -4,6 +4,6 @@ from gist_kakao_api import views
 
 urlpatterns = [
     path('', views.home),
-    path('drf-test-get', views.drf_test_get),
+    # path('drf-test-get', views.drf_test_get),
     path('drf-test-post', views.drf_test_post),
 ]
